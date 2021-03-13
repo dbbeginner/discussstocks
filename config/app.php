0cac23fa76a8412c7f5da90fbe5e7ffb02fb6e84
Lucas Krupinski <lucasjkr@me.com>
2021-03-13 07:02:56 -0500

    Updated configs

--- config/app.php
+++ config/app.php
@@ -226,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Markdown' => Illuminate\Mail\Markdown::class,
+        'Hashid' => Vinkla\Hashids\Facades\Hashids::class,
 
     ],
 
--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,12 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'static' => [
+            'driver' => 'local',
+            'root' => storage_path('static'),
+        ],
+
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
