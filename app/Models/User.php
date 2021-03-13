a663af7d40bf25340926a0c4ae5468af791ebd67
Lucas Krupinski <lucasjkr@me.com>
2021-03-13 07:37:05 -0500

    Update User.php
    New user populates settings with the defaults from the Guest user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -20,6 +21,15 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+    ];
+
+    /**
+     * Default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'active' => false,
     ];
 
     /**
@@ -40,4 +50,48 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            $post->token = (string) Str::uuid();
+        });
+
+        static::saved( function($model) {
+            $defaultSettings = \App\Models\Settings::where('user_id', '=', '1')->get();
+
+            foreach($defaultSettings as $setting){
+                $save = new \App\Models\Settings;
+                $save->user_id = $model->id;
+                $save->setting = $setting->setting;
+                $save->value = $setting->value;
+                $save->save();
+            }
+        });
+    }
+
+    public function channel()
+    {
+        return $this->hasMany(Channels::class, 'user_id');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Posts::class, 'user_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Replies::class, 'user_id');
+    }
+    public function votes()
+    {
+        return $this->hasMany(Votes::class, 'user_id');
+    }
+
+    public function settings() {
+        return $this->hasMany(Settings::class, 'user_id');
+    }
 }
