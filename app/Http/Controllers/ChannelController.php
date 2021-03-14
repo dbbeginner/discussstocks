6e8400ba8255a2beb4c243090336961c9d664b90
Lucas Krupinski <lucasjkr@me.com>
2021-03-14 09:46:56 -0400

    Add scope to Channel model

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Channels;
+use App\Models\Content;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ChannelController extends Controller
 {
     //
 
-    public function all()
-    {
+
+    public function AllChannels() {
+
         $data['channels'] = Channels::where('deleted_at', '=', null)
-            ->where('type', '=', 'channel')
-            ->withCount('posts')->orderBy('updated_at')->simplePaginate();
+            ->withCount('posts')
+            ->orderByDesc('updated_at')
+            ->simplePaginate();
 
         return view('channels', $data);
+
     }
 
     public function create()
@@ -37,8 +41,6 @@ class ChannelController extends Controller
         $channel->content = $request->post('description');
         $channel->user_id = Auth::user()->id;
         $channel->save();
-
-//        $result = Channels::where('id', '=', $channel->id)->first();
 
         return redirect()->back()->with('success', 'Your channel named <a href="' . $channel->url() .'">' . $channel->title . '</a> was created.');
 
--- app/Models/Channels.php
+++ app/Models/Channels.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Scopes\ChannelScope;
 
 // The Channels model extends the Content model and stores its data in the Content table.
+// Scope insures that calls to All channels returns only Channels, and not other data stored in the database.
 
 class Channels extends Content
 {
@@ -19,7 +20,11 @@ class Channels extends Content
 
     protected static function boot()
     {
+
+        static::addGlobalScope(new ChannelScope());
+
         parent::boot();
+
         static::saving(function($model){
         });
 
