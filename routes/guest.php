a1f34968451e4d8a0a1d66e3145d1e2f5c09e708
Lucas Krupinski <lucasjkr@me.com>
2021-03-16 23:07:15 -0400

    Update guest.php
    Guest routes

--- routes/guest.php
+++ routes/guest.php
@@ -5,29 +5,35 @@ use Illuminate\Support\Facades\Route;
 // Routes that unauthenticated users can use
 // In a separate file just to reduce clutter
 
+// Homepage
 Route::get('/', '\App\Http\Controllers\PostController@recentPosts')->name('home');
 
+// Static pages
 Route::get('/terms', '\App\Http\Controllers\StaticController@Terms');
 Route::get('/about', '\App\Http\Controllers\StaticController@About');
 Route::get('/markdown', '\App\Http\Controllers\StaticController@markdown');
 
-
+// Registration pages
 Route::get('/register', '\App\Http\Controllers\Accounts\RegistrationController@Create');
 Route::post('/register', '\App\Http\Controllers\Accounts\RegistrationController@Register');
 
-
-Route::get('/c/{channel_slug}/{channel_hash_id}/{post_slug}/{post_hash_id}', '\App\Http\Controllers\PostController@viewPost');
-
-
-Route::get('/c/{slug}', '\App\Http\Controllers\Guesser@Channel');
-Route::get('/p/{slug}', '\App\Http\Controllers\Guesser@Post');
-
-Route::get('/c/{slug}/{hashid}', '\App\Http\Controllers\PostController@ViewPostsInChannel');
-Route::get('/p/{slug}/{hashid}', 'ViewController@PostWithReplies');
-
-Route::get('/channels', '\App\Http\Controllers\ChannelController@AllChannels');
-
+// Login/Logout pages
 Route::get('login', function(){ return view('login'); })->name('login');
 Route::post('login', 'App\Http\Controllers\Accounts\LoginController@authenticate');
 
-Route::get('{hash_id', '\App\Http\Controllers\Guesser@GuessByHashId');
+// List of channels
+Route::get('/channels', '\App\Http\Controllers\ChannelController@AllChannels');
+
+// View a single channel
+Route::get('/c/{slug}/{hash_id}', '\App\Http\Controllers\PostController@ViewPostsInChannel');
+
+// View a single story in a channel
+Route::get('/c/{channel_slug}/{channel_hash_id}/{post_slug}/{post_hash_id}', '\App\Http\Controllers\PostController@viewPost');
+
+// ShortURL straight to channel or post (for social sharing)
+Route::get('{hash_id}', '\App\Http\Controllers\Guesser@GuessByHashId');
+
+
+
+
+
