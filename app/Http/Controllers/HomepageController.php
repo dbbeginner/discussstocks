fb937638a4c619d1fa6bf6f3b8d28a53263d37f5
Lucas Krupinski <lucasjkr@me.com>
2021-03-18 20:21:58 -0400

    Update HomepageController.php
    Homepage Controller!
    
    By default, Guest users gets ALL posts, while Registered users get posts only from the channels they're subscribed to

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Content;
+use App\Models\Posts;
+use App\Models\Subscriptions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\PostController;
@@ -12,8 +15,39 @@ class HomepageController extends Controller
 
     public function index(PostController $postController) {
         if(Auth::guest()) {
-            return $postController->recentPosts();
+            $data['posts'] = $this->AllRecentPosts();
+            $data['title'] = 'All Recent Posts';
+        } else {
+            $data['posts'] = $this->PostsFromUsersSubscriptions();
+            $data['title'] = 'Recent Posts Based On Your Preferences';
         }
-        return "registered home page";
+
+        return view('posts', $data);
+    }
+
+    public function AllRecentPosts(){
+
+        return Posts::where('type', '=', 'post')
+            ->with('user', 'votes')
+            ->withCount('votes')
+            ->withSum('votes', 'vote')
+            ->orderByDesc('updated_at')
+            ->simplePaginate( setting('pagination', 10) );
+
+    }
+
+
+
+    public function PostsFromUsersSubscriptions() {
+        // Get list of channels the current logged in user is subscribed to
+        $subs = Subscriptions::where('user_id', Auth::user()->id)->pluck('content_id');
+
+        Return Posts::whereIn('parent_id', $subs)
+            ->with('user', 'votes')
+            ->withCount('votes')
+            ->withSum('votes', 'vote')
+            ->orderByDesc('updated_at')
+            ->simplePaginate( setting('pagination', 10));
+
     }
 }
