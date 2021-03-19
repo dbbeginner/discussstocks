b3aa7e6003bea28bd52b3c3108b47039afcc8821
Lucas Krupinski <lucasjkr@me.com>
2021-03-19 08:41:58 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+require __DIR__.'/guest.php';
+
+
+Route::get('logout', 'App\Http\Controllers\Accounts\LoginController@logout')
+    ->middleware('auth');
+
+
+Route::group(['middleware' => 'auth'], function () {
+    // Paths for registered users to create and delete channels
+    Route::group(['prefix' => 'channel'], function() {
+        Route::get('create', 'App\Http\Controllers\ChannelController@create');
+        Route::post('create', 'App\Http\Controllers\ChannelController@store');
+    });
 });
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'user'], function() {
+//        Manage user settings
+        Route::get('settings', 'App\Http\Controllers\UserSettingsController@view');
+        Route::post('settings', 'App\Http\Controllers\UserSettingsController@store');
+
+//        Manage channel subscriptions
+        Route::get('subscriptions', 'App\Http\Controllers\UserSubscriptionsController@index');
+        Route::post('subscriptions/save', 'App\Http\Controllers\UserSubscriptionsController@store');
+    });
+});
+
+
+
+Route::post('/vote/', '\App\Http\Controllers\VoteController@CastVote');
+Route::get('/vote/', '\App\Http\Controllers\VoteController@CastVote');
+
+
+
+
+//require __DIR__.'/auth.php';
+
+
+Route::get('/p/{slug}/{hashid}', [\App\Http\Controllers\PostController::class, 'viewPost']);
+Route::get('/r/{slug}/{hashid}', [\App\Http\Controllers\ReplyController::class, 'viewReply']);
+Route::get('/u/{username}', [\App\Http\Controllers\UserController::class, 'index']);
+Route::get('/u/{username}/posts', [\App\Http\Controllers\UserController::class, 'index']);
+Route::get('/u/{username}/replies', [\App\Http\Controllers\UserController::class, 'index']);
+
+
+
+
+// ShortURL straight to channel or post (for social sharing)
+// This has got to be the last route, because it matches with everything else.
+Route::get('{hash_id}', '\App\Http\Controllers\Guesser@GuessByHashId');
