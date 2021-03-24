fe8cbf96a478554727a0eee5bbb6e825efb9f3cf
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-03-24 11:40:57 -0400

    API keys from different data providers

---
+++ config/api.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    // Alphavantage API key
+    // Developer Docs: https://www.alphavantage.co/documentation/
+    'AV' => env('AV_KEY', 'demo'),
+
+    // Financial Modelling Prep API Key
+    // Developer Docs: https://financialmodelingprep.com/developer/docs
+    'FMP' => env('FMP_KEY', 'demo'),
+
+];
+
