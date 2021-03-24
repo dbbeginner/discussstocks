6d2db0cdef84b246a45b773b0be518e45e6d1798
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-03-24 01:33:22 -0400

    Add routes and placeholder controllers

---
+++ app/Http/Controllers/Channel/ChannelSettings.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Channel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class ChannelSettings extends Controller
+{
+    //
+
+    public function index(Request $request) {
+
+    }
+    public function store(Request $request){
+    }
+
+
+}
