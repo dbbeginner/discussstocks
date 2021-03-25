b1e23f8abfc766f530879dbf4e456ae7266cb1b1
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-03-25 06:22:09 -0400

    Update reply.blade.php

--- resources/views/template/content/reply.blade.php
+++ resources/views/template/content/reply.blade.php
@@ -6,15 +6,13 @@
             <li>
                 <div style="display: inline-block; width: calc(100% - 30pt); margin-right: 3px; padding: 0px;">
 
-                    ID {{ $reply->id }} / Parent {{ $reply->parent_id }}
                     <p> <strong>{{ $reply->user->name }}</strong> said
                         at {{ $reply->created_at->format( 'h:i a' ) }}
                         on {{ $reply->created_at->format( 'm/d/Y' ) }} </p>
-
-                    {!! $render->markdownToHtml( $reply->content ) ?? "" !!}
-
-                    <p>{!! $reply->formattedContent() !!}</p>
                     <p>
+                    {!! $render->markdownToHtml( $reply->content ) ?? "" !!}
+                    <p>
+
                     <div class="align-top" style="border-width: 0px; border-style: solid; display: inline-block; width: 40pt; margin: 0px; padding: 0px; background: #00459b; text-align: center; !important; color: #fff;">
                         <span style="font-size: 6pt; baseline-shift: 3pt;">
                             {{ ($reply->total_upvotes + $reply->votes_sum_vote) }}
@@ -22,6 +20,8 @@
                         <i class="far fa-angle-down" style="position: relative; top: 2pt; float: right; margin-right: 5pt;"></i>
                         <i class="far fa-angle-up" style="position: relative; top: 2pt; float: right; margin-right: 3pt; "></i>
                     </div>
+                    Add Reply
+                    {{ $reply->parentByType('post')->id }}
                     </p>
                 </div>
             </li>
