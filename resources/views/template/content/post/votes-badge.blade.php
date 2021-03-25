73d483efe80d71450a0bc55dbc0fd39cc43da0c9
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-03-25 09:45:04 -0400

    Update votes-badge.blade.php
    Resolve division by zero error that occurs if a post has no votes (only occurs when the person that makes the post downvotes it themselves)

--- resources/views/template/content/post/votes-badge.blade.php
+++ resources/views/template/content/post/votes-badge.blade.php
@@ -7,7 +7,11 @@
     @endif
         <div style="display: table-cell;  text-align: center; width: 40pt; vertical-align: middle; line-height: 8pt; height: 32px; border-radius: 16pt; background: #ff0000; color: #fff; font-size: 9pt;">
             <strong id="percent-{{ $post->hashId() }}">
+                @if($post->total_upvotes + $post->votes_sum_vote == 0 || $post->total_votes + $post->votes_count == 0)
+                    0%
+                @else
                 {{ round(($post->total_upvotes + $post->votes_sum_vote) / ($post->total_votes + $post->votes_count) * 100, 0) }}%
+                @endif
             </strong><br>
             <small>upvoted</small>
         </div>
