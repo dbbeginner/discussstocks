0e107eddda9106f44fc45ffe4f4fe7afdae75fd5
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-03-26 15:31:04 -0400

    Create 2021_03_25_210328_add_user_profile_fields_to_users_table.php

---
+++ database/migrations/2021_03_25_210328_add_user_profile_fields_to_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserProfileFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('image')
+                ->after('name')
+                ->nullable();
+            $table->longText('bio')
+                ->after('image')
+                ->nullable();
+            $table->boolean('display_email')
+                ->after('email')
+                ->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('image');
+            $table->dropColumn('bio');
+            $table->dropColumn('display_email');
+        });
+    }
+}
