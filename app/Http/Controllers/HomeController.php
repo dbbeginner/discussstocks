f65a0882ba58e8ef6e00b126e812a97b335d57a9
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-03-28 07:45:13 -0400

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Channel;
 use App\Models\Post;
-use App\Models\Subscriptions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Vinkla\Hashids\Facades\Hashids;
@@ -23,11 +22,8 @@ class HomeController extends Controller
     }
 
     public function allPosts(){
-        $query = Post::where('type', '=', 'post')
-            ->with('user', 'votes')
-            ->withCount('votes')
-            ->withSum('votes', 'vote')
-            ->orderByDesc('updated_at');
+        $query = Post::where('type', '=', 'post');
+
         $title = 'All Recently Posted';
 
         return $this->renderView($query, $title);
@@ -41,32 +37,16 @@ class HomeController extends Controller
             return redirect('/user/subscriptions')->with('info', "You're not subscribed to any channels yet. Pick a few below");
         }
 
-        // If user is only subscribed to one channel, load just that channels posts
-        if(count($subscriptions) == 1 ) {
-            $query = Post::where('parent_id', '=', $subscriptions->first() )
-                ->with('user', 'votes')
-                ->withCount('votes')
-                ->withSum('votes', 'vote')
-                ->orderByDesc('updated_at');;
-        } else {
-        // If the user is subscribed to more than one channel, show the posts from all those channels
-            $query = Post::whereIn('parent_id', $subscriptions )
-                ->with('user', 'votes')
-                ->withCount('votes')
-                ->withSum('votes', 'vote')
-                ->orderByDesc('updated_at');;
-        }
+        $query = Post::whereIn('parent_id', $subscriptions );
+
         $title = 'Recent Posts Based On Your Preferences';
         return $this->renderView($query, $title);
     }
 
     public function postsInChannel(Request $request, $slug, $hashId){
         $channel = Channel::where('id', '=', Hashids::decode($hashId))->first();
-        $query = Post::where('parent_id', '=', $channel->id)
-            ->with('user', 'votes')
-            ->withCount('votes')
-            ->withSum('votes', 'vote')
-            ->orderByDesc('updated_at');;
+        $query = Post::where('parent_id', '=', $channel->id);
+
         $title = 'Posts in ' . $channel->title;
 
         return $this->renderView($query, $title, $channel, 'channel');
@@ -74,19 +54,16 @@ class HomeController extends Controller
 
     public function renderView($data, $title = null, $channel = null, $blade = 'posts') {
         $count = $data->count();
-        $content = $data->simplePaginate( preference('pagination', 10) );
-
-        if( $count < preference('pagination') - 1) {
-            $notice = "There aren't many posts here. Perhaps you want to <a href=\"/user/subscriptions\">subscribe</a> to more channels?";
-        } else {
-            $notice = null;
-        }
+        $content = $data->with('user', 'votes')
+            ->withCount('votes')
+            ->withSum('votes', 'vote')
+            ->orderByDesc('updated_at')
+            ->simplePaginate( preference('pagination', 10) );
 
         return view($blade, [
             'title' => $title,
             'posts' => $content,
             'count' => $count,
-            'notice' => $notice,
             'channel' => $channel,
             ]);
     }
