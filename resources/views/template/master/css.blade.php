ede4323c89b7381f2ab6ac21832e8f9b29ab1f58
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-04-09 17:02:05 -0400

    user preferences set where to load dependencies from

--- resources/views/template/master/css.blade.php
+++ resources/views/template/master/css.blade.php
@@ -1,4 +1,10 @@
 <!-- Stylesheets -->
+@if( preference('assets', 'hosted') == 'hosted')
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+@else
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/fontawesome-all.min.css">
+@endif
+
 <link rel="stylesheet" href="/css/custom.css?{{ rand(0, 10000000) }}">
