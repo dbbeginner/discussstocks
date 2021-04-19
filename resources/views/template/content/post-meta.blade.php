3235b7a30b0c45a06500843d003e27284f794f35
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-04-19 17:01:21 -0400

    Update post-meta.blade.php
    Set the post meta links to buttons with btn-link styling.

--- resources/views/template/content/post-meta.blade.php
+++ resources/views/template/content/post-meta.blade.php
@@ -24,11 +24,13 @@
         <a href="{{ $post->url() }}">{{ $post->reply_count }} replies</a>
     </li>
     <li>
-        <a id="share-{{ $post->hashId() }}" class="pseudo-link" onclick="shareLink('{{ $post->hashId() }}')">Share link</a>
+        <button class="btn btn-link" style="margin:0pt; margin-top: -2pt; padding:0pt; font-size: 8pt; top: 0pt;"id="share-{{ $post->hashId() }}"
+             class="pseudo-link" onclick="shareLink('{{ $post->hashId() }}')">Share link</button>
     </li>
     @if( Auth::check() )
     <li>
-        <a onclick="showReportContentModal('{{ $post->hashId()}}', '{{ Auth::user()->hashId() }}')">Report this </a>
+        <button class="btn btn-link" style="margin:0pt; margin-top: -2pt; padding:0pt; font-size: 8pt; top: 0pt;"
+                onclick="showReportContentModal('{{ $post->hashId()}}', '{{ Auth::user()->hashId() }}')">Report this </button>
     </li>
     @endif
     <li>
