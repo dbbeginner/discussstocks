e719702510416baebd0d5f1fe518d0ae8f04e806
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-04-19 17:00:32 -0400

    Update activation-resend.blade.php
    Change destination URL to /verify from /activate

--- resources/views/mail/activation-resend.blade.php
+++ resources/views/mail/activation-resend.blade.php
@@ -7,7 +7,7 @@
 <body>
 <h2>Your account at {{ config('app.name') }}</h2>
 <br/>
-Please follow this <a href="{{ config('app.url') }}/activate?token={{ $user->token }}">link</a> to activate your account on {{ config('app.name') }}. <br>
+Please follow this <a href="{{ config('app.url') }}/verify?token={{ $user->token }}">link</a> to activate your account on {{ config('app.name') }}. <br>
 If the link doesn't work, please visit {{ config('app.url') }} and paste the following confirmation code:<br>
 <br>
 <strong>{{ $user->token }}</strong><br>
