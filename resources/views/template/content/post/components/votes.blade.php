9d42554bbe463502330e73aa592130b6452584cf
Lucas Krupinski <80363381+dbbeginner@users.noreply.github.com>
2021-04-23 15:56:23 -0400

    Vote styling
    color updates

--- resources/views/template/content/post/components/votes.blade.php
+++ resources/views/template/content/post/components/votes.blade.php
@@ -1,13 +1,25 @@
 <div class="votes-container">
+    @if(Auth::check())
     <button class="vote vote-up" onclick="upvote('{{ $post->hashId() }}', '{{ Auth::user()->hashId() }}', 'up')">
         <i class="fa fa-caret-up"></i>
     </button>
+    @else
+    <button class="vote vote-up" disabled>
+        <i class="fa fa-caret-up"></i>
+    </button>
+    @endif
     <br>
     <span class="vote-count" id="votesum-{{ $post->hashId() }}">
         {{ $post->sumOfVotes() }}
    </span>
     <br>
+    @if(Auth::check())
     <button class="vote vote-down" onclick="upvote('{{ $post->hashId() }}', '{{ Auth::user()->hashId() }}', 'down')">
         <i class="fa fa-caret-down"></i>
     </button>
+    @else
+    <button class="vote vote-down" disabled>
+        <i class="fa fa-caret-down"></i>
+    </button>
+    @endif
 </div>
